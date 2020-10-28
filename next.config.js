fbe810e8d6db60f794b9cc1cbf3d1ca6848e8c07
Richard Kelly II <richard.kelly.ii@gmail.com>
2020-10-28 12:49:34 -0400

    add netlify

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/,
   })
   module.exports = withMDX({
+    target: "serverless",
     options: {
       remarkPlugins: [require('remark-unwrap-images')],
     },
